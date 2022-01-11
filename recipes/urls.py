b3f2e7bba20b112bfb00ba8afe01dea674fb69e8
Crystal Faqiri <cfaqiri@gmail.com>
2022-01-10 20:48:47 -0500

    testing password reset email functionality

--- recipes/urls.py
+++ recipes/urls.py
@@ -11,7 +11,13 @@ urlpatterns = [
     path("add/", views.add_recipe, name="add_recipe"),
     path("search/", views.search, name="search"),
     path("<int:user_id>/recipes/", views.my_recipes, name="my_recipes"),
-    path("password-reset/",
+    path("password_reset/",
         auth_views.PasswordResetView.as_view(template_name="recipes/password_reset_form.html"),
-        name="password_reset")
+        name="password_reset"),
+    path("password_reset/done/",
+        auth_views.PasswordResetDoneView.as_view(template_name="recipes/password_reset_done.html"),
+        name="password_reset_done"),
+    path("password_reset/confirm/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(template_name="recipes/password_reset_confirm.html"),
+        name="password_reset_confirm"),
 ]
