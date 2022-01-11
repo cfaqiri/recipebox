a0260f1e6cb10c612b27519d56dd364222375145
Crystal Faqiri <cfaqiri@gmail.com>
2022-01-10 20:00:12 -0500

    testing password reset form

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,14 +5,13 @@ from recipes import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("login", views.login_view, name="login"),
-    path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register"),
-    path("add", views.add_recipe, name="add_recipe"),
-    path("search", views.search, name="search"),
-    path("<int:user_id>/recipes", views.my_recipes, name="my_recipes"),
-    path("password-reset",
-        auth_views.PasswordResetView.as_view(template_name="password_reset.html"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("register/", views.register, name="register"),
+    path("add/", views.add_recipe, name="add_recipe"),
+    path("search/", views.search, name="search"),
+    path("<int:user_id>/recipes/", views.my_recipes, name="my_recipes"),
+    path("password-reset/",
+        auth_views.PasswordResetView.as_view(template_name="recipes/password_reset_form.html"),
         name="password_reset")
-
 ]
