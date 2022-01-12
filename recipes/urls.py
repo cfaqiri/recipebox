1116654cddf144245d6c8c98ad12b3072679df3b
Crystal Faqiri <cfaqiri@gmail.com>
2022-01-11 21:09:24 -0500

    changed resetcomplete view to custom

--- recipes/urls.py
+++ recipes/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
         auth_views.PasswordResetConfirmView.as_view(),
         name="password_reset_confirm"),
     path("password_reset/complete/",
-        auth_views.PasswordResetCompleteView.as_view(),
+        auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_complete.html"),
         name="password_reset_complete")
 ]
